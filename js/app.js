cbf6d327fa55e31c4b4c14a5b0e0498c14211ec8
Yu Harin <yuharin0707@naver.com>
2023-03-31 11:21:43 +0900

    2. 레이아웃 설정 및 가위바위보 버튼 구현

--- js/app.js
+++ js/app.js
@@ -1,25 +1,35 @@
 // 1. js 파일에서 접근해야하는 html dom 요소들 선언하기
+const myHand = document.getElementById('my-hand-text');
+const myHandIcon = document.getElementById('my-hand-icon');
+const computerHand = document.getElementById('computer-hand');
+
 const rockBtn = document.getElementById('rock')
 const scissorsBtn = document.getElementById('scissors')
 const paperBtn = document.getElementById('paper')
 
 
-//2. 내가 누른 버튼에 대한 정보 : ["누른 버튼", "icon class명"]
-const clickedBtnInfo = {
-    "rock": ["rock", "fa-regular fa-hand-back-fist"],
-    "scissors": ["scissors", "fa-regular fa-hand-scissors fa-rotate-90"],
-    "paper": ["paper", "fa-regular fa-hand"]
-}
-
-//3. 선언한 dom 요소에 클릭이벤트 부여하기
+//2. 선언한 dom 요소에 클릭이벤트 부여하기
 rockBtn.addEventListener('click', isClicked);
 scissorsBtn.addEventListener('click', isClicked);
 paperBtn.addEventListener('click', isClicked);
 
-//함수가 실행될 때, 이벤트(e) 객체에 접근하기
+//3. 함수가 실행될 때, 이벤트(e) 객체에 접근하기
+//4. innerHTML 실습
 function isClicked(e){
     let clickedBtn = e.currentTarget.id;
+    let clickedIcon = e.target;
+
+    //4)-1 innerHTML 실습할 때 typeof 사용하기
     console.log(clickedBtn);
+    console.log(clickedIcon);
+
+    myHand.innerText = clickedBtn;
+
+    //5. className 클릭 state 받아와서 className 갈아끼우기
+    myHandIcon.className = clickedIcon.className;
+    console.log(myHandIcon.className);
+
+
 }
 
 
